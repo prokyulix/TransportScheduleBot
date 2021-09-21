ec589d2c1259761b38b0246b54587675e7adeb08
Александр Самойлов <asamoylov@lanit.ru>
2021-09-21 19:33:44 +0300

    Initial commit

---
+++ src/main/java/com/prokyulix/bots/transport/TransportScheduleBotApplication.java
@@ -0,0 +1,13 @@
+package com.prokyulix.bots.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportScheduleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportScheduleBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prokyulix/bots/transport/TransportScheduleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokyulix.bots.transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportScheduleBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
